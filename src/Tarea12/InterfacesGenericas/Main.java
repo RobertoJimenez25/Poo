c46803bf6f320cbcd7ca0b0567eac15bcea4fa72
Robert Jimenez <robertojimenez75@aragon.unam.mx>
2023-04-29 19:03:32 -0600

    Se agrega la tarea 12 Interfaces graficas

---
+++ src/Tarea12/InterfacesGenericas/Main.java
@@ -0,0 +1,9 @@
+package Tarea12.InterfacesGenericas;
+
+import Tarea12.InterfacesGenericas.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
