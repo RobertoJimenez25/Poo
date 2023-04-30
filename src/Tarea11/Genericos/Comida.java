d67b38ac846529388cf5754b8cc11453c6503725
Robert Jimenez <robertojimenez75@aragon.unam.mx>
2023-04-29 18:12:04 -0600

    Se agrega la tarea 11 Genericos

---
+++ src/Tarea11/Genericos/Comida.java
@@ -0,0 +1,38 @@
+package Tarea11.Genericos;
+
+public class Comida {
+    private String nombre;
+    private boolean procesada;
+
+    public Comida() {
+    }
+
+    public Comida(String nombre, boolean procesasa) {
+        this.nombre = nombre;
+        this.procesada = procesada;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isProcesada() {
+        return procesada;
+    }
+
+    public void setProcesada(boolean procesada) {
+        this.procesada = procesada;
+    }
+
+    @Override
+    public String toString() {
+        return "Comida{" +
+                "nombre='" + nombre + '\'' +
+                ", procesada=" + procesada +
+                '}';
+    }
+}
