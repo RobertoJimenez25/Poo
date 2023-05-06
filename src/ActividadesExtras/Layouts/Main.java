a8ea3808cbbc50d7811728732300787ebc4bd35b
Robert Jimenez <robertojimenez75@aragon.unam.mx>
2023-05-05 22:05:20 -0600

    Se agrega la Actividad extra 3

---
+++ src/ActividadesExtras/Layouts/Main.java
@@ -0,0 +1,9 @@
+package ActividadesExtras.Layouts;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout cuadriculada");
+    }
+}
